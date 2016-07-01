536b14be62d861032ace7700dadd5424ae3f4999
Michael Mesereau <mmesereau1@gmail.com>
2016-07-01 15:33:55 -0600

    logout functionality

--- routes/logout.js
+++ routes/logout.js
@@ -5,4 +5,9 @@ var cookieSession = require('cookie-session');
 var knex = require('../db/knex');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+  req.session = null;
+  res.redirect('/');
+});
+
 module.exports = router;
