209c048b051a3ac70035c47aaaf8b79c6cb2697e
Michael Mesereau <mmesereau1@gmail.com>
2016-07-05 15:54:44 -0600

    add recipe functionality

--- routes/recipes.js
+++ routes/recipes.js
@@ -14,4 +14,38 @@ router.get('/new', function(req, res) {
   res.render('addnewrecipe');
 });
 
+router.post('/new', function(req, res) {
+  var newrecipe = req.body.newrecipe.toLowerCase();
+  var category = req.body.category.toLowerCase();
+  knex.select('id').from('households').where('name', req.session.household)
+  .then(function(house) {
+    knex('recipes').where('name', newrecipe)
+    .then(function(recipe) {
+      if (recipe[0]) {
+        knex('households-recipes').where({households_id: house[0].id, recipes_id: recipe[0].id})
+        .then(function(existing) {
+          console.log(existing);
+          if (!existing[0]) {
+            knex('households-recipes').insert({households_id: house[0].id, recipes_id: recipe[0].id})
+            .then(function() {
+              res.redirect('/');
+            });
+          }
+        });
+      }
+      else {
+        knex('recipes').insert({name: newrecipe, category: category})
+        .then(function() {
+          knex.select('id').from('recipes').where(name, newrecipe)
+          .then(function(recipe){
+            knex('households-recipes').insert({households_id: house[0].id, recipes_id: recipe[0].id})
+            .then(function() {
+              res.redirect('/');
+            });
+          });
+        });
+      };
+    });
+  });
+});
 module.exports = router;
