095104bd36435ec78c14c87a0fb9945640bc1cee
Michael Mesereau <mmesereau1@gmail.com>
2016-07-05 14:04:28 -0600

    ability to create new households

--- routes/newhousehold.js
+++ routes/newhousehold.js
@@ -6,5 +6,13 @@ var knex = require('../db/knex');
 var router = express.Router();
 
 
+router.get('/', function(req, res) {
+  res.render('createHousehold.ejs');
+});
 
+router.post('/', function(req, res) {
+  knex('households').insert({name: req.body.name});
+  req.session.household = req.body.name;
+  res.redirect('/');
+});
 module.exports = router;
