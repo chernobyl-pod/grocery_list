5ffbf23520b15a8e57a4b36fa240d96cae132a35
Michael Mesereau <mmesereau1@gmail.com>
2016-07-05 09:58:40 -0600

    login functionality

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   if (req.session.name) {
-    res.render('myhouse');
+    res.render('myhouse', {groceries: []});
     console.log(req.session);
   } else {
     console.log(req.session);
