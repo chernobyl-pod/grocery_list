4f2b05d6281ffe650d06cb7c545272efdb09d5e3
Michael Mesereau <mmesereau1@gmail.com>
2016-07-05 10:20:52 -0600

    households route

--- routes/households.js
+++ routes/households.js
@@ -6,6 +6,7 @@ var knex = require('../db/knex');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+  knex.from('members').innerJoin('households-members', 'members.id', 'households-members.members_id').innerJoin('households', 'households-members.households_id', 'households.id');
   res.render('householdList');
 });
 module.exports = router;
