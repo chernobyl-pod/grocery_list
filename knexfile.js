70b2a5c6b24cd3decb75e376a1f05335d23a3014
Roy <rechase84@gmail.com>
2016-07-05 14:05:35 -0600

    some sass

--- knexfile.js
+++ knexfile.js
@@ -21,19 +21,8 @@ module.exports = {
   },
 
   production: {
-<<<<<<< HEAD
     client: 'postgres',
     connection: process.env.DATABASE_URL
-=======
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
->>>>>>> f2462a17272f26e060cc70e8165dfa14124c70b9
+
   }
 };
