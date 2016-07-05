4659af414817b28f8f0d28377c95b1ea9fb7c89a
Charles Stafford <cstaff@Charless-MacBook-Pro.local>
2016-07-05 10:41:42 -0600

    seed files

--- seeds/recipes-food.js
+++ seeds/recipes-food.js
@@ -5,10 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('recipes-food').insert({recipe_id: 1, food_id: 1}),
-        knex('recipes-food').insert({recipe_id: 2, food_id: 2}),
-        knex('recipes-food').insert({recipe_id: 3, food_id: 3}),
-        knex('recipes-food').insert({recipe_id: 1, food_id: 4})
+        knex('recipes-food').insert({recipes_id: 1, food_id: 1}),
+        knex('recipes-food').insert({recipes_id: 2, food_id: 2}),
+        knex('recipes-food').insert({recipes_id: 3, food_id: 3}),
+        knex('recipes-food').insert({recipes_id: 1, food_id: 4})
       ]);
     });
 };
