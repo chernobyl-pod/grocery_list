df46e714e9e2257fb7d2191d629fa189b592bbbf
Michael Mesereau <mmesereau1@gmail.com>
2016-07-05 10:02:14 -0600

    started households route

---
+++ routes/households.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var express = require('express');
+var cookieSession = require('cookie-session');
+var knex = require('../db/knex');
+var router = express.Router();
+
+
+module.exports = router;
