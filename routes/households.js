ecc4d04cd99e58c9772daabe179709d88c7f7a58
Michael Mesereau <mmesereau1@gmail.com>
2016-07-05 10:07:24 -0600

    message

--- routes/households.js
+++ routes/households.js
@@ -5,5 +5,7 @@ var cookieSession = require('cookie-session');
 var knex = require('../db/knex');
 var router = express.Router();
 
-
+router.get('/', function(req, res) {
+  res.render('householdList');
+});
 module.exports = router;
