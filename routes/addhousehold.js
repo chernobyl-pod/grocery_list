1b779f33342595bfd4d1ca0663ef06a2e0eee2ab
Michael Mesereau <mmesereau1@gmail.com>
2016-07-05 11:54:11 -0600

    households-members id bug

--- routes/addhousehold.js
+++ routes/addhousehold.js
@@ -16,7 +16,7 @@ router.post('/:id', function(req, res) {
   console.log("Hello");
   knex.select('id').from('members').where('name', req.session.name).then(function(data) {
     console.log(data, req.params.id);
-    knex('households-members').insert([{households_id: req.params.id}, {members_id: data[0].id}]).then(function(data) {
+    knex('households-members').insert([{households_id: req.params.id, members_id: data[0].id}]).then(function(data) {
       res.redirect('/');
     })
   });
