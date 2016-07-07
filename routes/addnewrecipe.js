3e9fe35b752e7addf06886575cdb938c21b7db37
Michael Mesereau <mmesereau1@gmail.com>
2016-07-07 10:36:15 -0600

    bug fixes

--- routes/addnewrecipe.js
+++ routes/addnewrecipe.js
@@ -6,15 +6,22 @@ var knex = require('../db/knex');
 var router = express.Router();
 
 router.post('/', function(req, res) {
-  knex('recipes').insert([{name: req.body.recipe_name, category: req.body.recipe_style, allergies: req.body.recipe_allergies, preferred_diet: req.body.recipe_preferred_diet, course: req.body.recipe_course}])
-  .then(function() {
-    req.session.recipe = req.body.recipe_name;
-    res.render('createrecipe', {recipe: req.body.recipe_name, ingredients: []});
+  knex('recipes').where('name', req.body.recipe_name)
+  .then(function(existing) {
+    if (!existing[0]) {
+      knex('recipes').insert([{name: req.body.recipe_name, category: req.body.recipe_style, allergies: req.body.recipe_allergies, preferred_diet: req.body.recipe_preferred_diet, course: req.body.recipe_course}])
+      .then(function() {
+        req.session.recipe = req.body.recipe_name;
+        res.render('createrecipe', {recipe: req.body.recipe_name, ingredients: []});
+      });
+    }
+    else {
+      res.redirect('/recipes');
+    }
   });
 });
 
 router.get('/', function(req, res) {
-  console.log(req.session);
   knex('recipes').where('name', req.session.recipe)
   .then(function(recipe) {
     knex('recipes-food').where('recipes_id', recipe[0].id)
@@ -65,15 +72,36 @@ router.post('/addingredient', function(req, res) {
   });
 });
 
+router.post('/deleteingredient/:ingredient', function(req, res) {
+  knex('food').where('name', req.params.ingredient)
+  .then(function(ingredient) {
+    knex('recipes').where('name', req.session.recipe)
+    .then(function(recipe) {
+      knex('recipes-food').where('recipes_id', recipe[0].id).where('food_id', ingredient[0].id).del()
+      .then(function() {
+        res.redirect('/addnewrecipe');
+      })
+    })
+  })
+})
+
 router.post('/submit', function(req, res) {
   knex('households').where('name', req.session.household)
   .then(function(house) {
     knex('recipes').where('name', req.session.recipe)
     .then(function(recipe) {
-      knex('households-recipes').insert([{households_id: house[0].id, recipes_id: recipe[0].id}])
-      .then(function() {
-        res.redirect('/recipes');
-      });
+      knex('households-recipes').where('households_id', house[0].id).where('recipes_id', recipe[0].id)
+      .then(function(existing) {
+        if (!existing[0]) {
+          knex('households-recipes').insert([{households_id: house[0].id, recipes_id: recipe[0].id}])
+          .then(function() {
+            res.redirect('/recipes');
+          });
+        }
+        else {
+          res.redirect('/recipes');
+        }
+      })
     });
   });
 });
