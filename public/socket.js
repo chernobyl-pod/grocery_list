d06d39a080bc0637e140ac7f4711f943966bfc02
Roy <rechase84@gmail.com>
2016-07-07 15:02:05 -0600

    stuff

--- public/socket.js
+++ public/socket.js
@@ -4,9 +4,9 @@ var socket = io();
 var items = document.getElementById('socketMessages');
 socket.on('new item', function(data) {
     console.log("data:", data);
-    var h1 = document.createElement('h3');
-      h1.innerHTML = data.name;
-    items.appendChild(h1);
+    var h3 = document.createElement('h3');
+      h3.innerHTML = data.name;
+    items.appendChild(h3);
     //socket.emit('here', {my: 'data'});
 });
 
